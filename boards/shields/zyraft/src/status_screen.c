4ddcd9152d3abcc7417336c22e05fc4f77100e44
falbatech <32847945+falbatech@users.noreply.github.com>
2026-05-20 08:09:54 +0200

    Update status_screen.c

--- boards/shields/zyraft/src/status_screen.c
+++ boards/shields/zyraft/src/status_screen.c
@@ -224,12 +224,12 @@ static void build_battery_widgets(void) {
     set_hidden(right_percent, true);
 
     left_icon = lv_label_create(screen);
-    style_text(left_icon, COLOR_CHARGE, &lv_font_montserrat_16);
+    style_text(left_icon, COLOR_CHARGE, &lv_font_montserrat_14);
     lv_obj_align(left_icon, LV_ALIGN_CENTER, -76, -28);
     set_hidden(left_icon, true);
 
     right_icon = lv_label_create(screen);
-    style_text(right_icon, COLOR_CHARGE, &lv_font_montserrat_16);
+    style_text(right_icon, COLOR_CHARGE, &lv_font_montserrat_14);
     lv_obj_align(right_icon, LV_ALIGN_CENTER, 76, -28);
     set_hidden(right_icon, true);
 
